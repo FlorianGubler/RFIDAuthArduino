e0d1750ce0a5fc19a0416f6f8ff876f7bfd61d25
Florian Gubler <gubler.florian@gmx.net>
2022-10-27 09:56:01 +0200

    Added Servo & Display Impl

--- main/main.ino
+++ main/main.ino
@@ -6,6 +6,34 @@
 
 MFRC522 mfrc522(PIN_SS, PIN_RESET);
 
+//Servo
+#include <Servo.h>
+Servo servo;  // create servo object to control a servo
+int pos = 0; 
+int opendeg = 90;
+
+//Display
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int color_defaultR = 0;
+const int color_defaultG = 0;
+const int color_defaultB = 255;
+
+const int color_errorR = 255;
+const int color_errorG = 0;
+const int color_errorB = 0;
+
+const int color_warnR = 255;
+const int color_warnG = 165;
+const int color_warnB = 0;
+
+const int color_validR = 0;
+const int color_validG = 255;
+const int color_validB = 0;
+
 //Wifi
 #include <WiFiNINA.h>
 
@@ -22,11 +50,7 @@ int port = 8080;          //Port of API
 WiFiClient client;
 
 void setup()
-{
-    while (!Serial) {
-      ; // wait for serial port to connect. Needed for native USB port only
-    }
-  
+{  
     // check for the WiFi module:
     if (WiFi.status() == WL_NO_MODULE) {
       Serial.println("Communication with WiFi module failed!");
@@ -51,6 +75,15 @@ void setup()
   
     Serial.println("Connected to wifi");
 
+    //Servo
+    servo.attach(7); 
+    Serial.println("Attached Servo");
+
+    //Display
+    lcd.begin(16, 2);
+    lcd.setRGB(color_defaultR, color_defaultG, color_defaultB);
+    printToDisplay("READY", 1, true);
+
     //Init RFID
     Serial.begin(9600);
     SPI.begin();
@@ -67,7 +100,6 @@ void loop()
             rfidUID += mfrc522.uid.uidByte[i];
         }
         verifyRFIDCard(rfidUID);
-        Serial.println();
         mfrc522.PICC_HaltA();
         delay(1000);
    }
@@ -82,15 +114,31 @@ void loop()
         
         if(ResponseCode.equals("200")){
             Serial.println("CARD VALID");
+            lcd.setRGB(color_validR, color_validG, color_validB);
+            printToDisplay("ACCESS GRANTED", 1, true);
+            delay(1000);
+            OpenWithServo();
         } else if(ResponseCode.equals("401")){
             Serial.println("CARD INVALID");
+            lcd.setRGB(color_errorR, color_errorG, color_errorB);
+            printToDisplay("ACCESS DENIED", 1, true);
+            delay(2000);
+            lcd.setRGB(color_defaultR, color_defaultG, color_defaultB);
+            printToDisplay("READY", 1, true);
         } else{
             Serial.println("UNKOWN RESPONSE");
+            lcd.setRGB(color_warnR, color_warnG, color_warnB);
+            printToDisplay("INTERNAL ERROR", 1, true);
+            printToDisplay("WAITING...", 1, false);
+            lcd.setCursor(0, 0);
         }
+        Serial.println("");
    }
 }
 
-boolean verifyRFIDCard(String rfid_UID){
+void verifyRFIDCard(String rfid_UID){
+    lcd.setRGB(color_defaultR, color_defaultG, color_defaultB);
+    printToDisplay("WAITING...", 1, true);
     Serial.println("Verifying RFID Card " + rfid_UID);
     // if you get a connection, report back via serial:
     if (client.connect(server, port)) {
@@ -101,5 +149,20 @@ boolean verifyRFIDCard(String rfid_UID){
     } else{
       Serial.println("Couldn't connect to server: " + String(server) + ":" + port);
     }
-    return false;
+}
+
+void OpenWithServo(){
+  for (pos = 0; pos <= opendeg; pos += 1) {
+    // in steps of 1 degree
+    servo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+}
+
+void printToDisplay(String inp, int line, boolean clear){
+  if(clear){
+      lcd.clear();
+  }
+  lcd.print(inp);
+  lcd.setCursor(0, line);
 }
